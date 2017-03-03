65183be347545f2eab412d5cb90cbfd5ec3c7ba5
Adam Bader <abade011@cgs-macd-16949.campus.goldsmiths.ac.uk>
2017-03-03 11:49:28 +0000

    update of algor

--- Algorithm/11/cpp/Node.cpp
+++ Algorithm/11/cpp/Node.cpp
@@ -53,47 +53,55 @@ unsigned int Node::length() {
 	int counter = 0;
 
 	if (list == Node::NIL) {
-		return 0;
+		return 0;//if list is NIL then return 0
 	}
 	else
+        //else, while the list is not NIL then access the next node
 		while (list != Node::NIL) {
 			list = list->getNext();
+            //increment counter by 1
 			counter++;
 		}
-	return counter;
+	return counter;//returns counter
 }
 
 Node *Node::remove(int i) {
+    //delList calls this
 	Node* delList = this;
 
 	if (delList == Node::NIL) {
-		return Node::NIL;
+		return Node::NIL;//if node is Node::NIL then return NIL
 	}
 	else if (delList->getItem() == i) {
+        //if delList (this) is i then move onto the next node
 		return delList->getNext()->remove(i);
 	}
 	else {
+        //else returns a new node with delList's value and removes the rest of the list and its next value
 		return new Node(delList->getItem(), delList->getNext()->remove(i));
 	}
 }
 
 Node *Node::reverse() {
-
-	if (this == Node::NIL) {
-		return Node::NIL;
+    //tempNode calls this and newList is has the varible NIL
+    Node* tempNode = this;
+    Node* newList = Node::NIL;
+    
+	if (tempNode == Node::NIL) {
+		return Node::NIL;//if node is Node::NIL then return NIL
 	}
-	else if (this->getNext() == Node::NIL) {
-		return new Node(this->getItem(), this->getNext());
+	else if (tempNode->getNext() == Node::NIL) {
+        //if tempNode then return a copy of the tempNode = this
+		return new Node(tempNode->getItem(), tempNode->getNext());
 	}
 	else {
-	Node* tempNode = this;
-	Node* newList = Node::NIL;
-
+    //else, while it is not the end of the list then tempNode is at the top
 	while (tempNode != Node::NIL) {
 		newList = new Node(tempNode->getItem(), newList);
 		tempNode = tempNode->getNext();
 
 	}
+    //this will return the newList
 	return newList;
 }
 }
